119afd0a7c960880a11558446193820f09f6d179
sworgo186 <sworgo2002@gmail.com>
2026-08-31 23:56:01 +0600

    update

--- controller/Loginvalidation.php
+++ controller/Loginvalidation.php
@@ -1,48 +1,93 @@
 <?php
+
 session_start();
+
 include "../Model/db.php";
+
  
+
 $name = "";
+
 $password = "";
+
 $message = "";
+
  
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
     $name = trim($_POST["name"] ?? "");
+
     $password = trim($_POST["password"] ?? "");
+
  
+
     $errors = [];
+
  
+
     if (empty($name)) {
+
         $errors[] = "User Name is required.";
+
     }
+
  
+
     if (empty($password)) {
+
         $errors[] = "Password is required.";
+
     }
+
  
+
     if (empty($errors)) {
+
         // OOP ক্লাসের মাধ্যমে কানেকশন তৈরি
+
         $database = new db();
+
         $conn = $database->connection();
+
  
-        // স্যারের মেথড বা কুয়েরি দিয়ে লগইন যাচাই
+
+        // স্যারের মেথড বা কুয়েরি দিয়ে লগইন যাচাই
+
         $result = $database->loginCheck($conn, "users", $name, $password);
+
  
+
         if ($result && $result->num_rows > 0) {
+
             $user = $result->fetch_assoc();
+
             $_SESSION["username"] = $user["username"];
-            
-            $conn->close();
+
            
+
+            $conn->close();
+
+           
+
             header("Location: ../View/delivery_dashboard.php");
+
             exit();
+
         } else {
+
             $message = "Invalid Username or Password!";
+
         }
+
         $conn->close();
+
     } else {
+
         $message = implode("<br>", $errors);
+
     }
+
 }
+
 ?>
- 
